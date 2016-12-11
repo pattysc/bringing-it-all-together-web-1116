62444b1e7c0b5908790b80cf64d74320caea8b2e
pattysc <pattys@users.noreply.github.com>
2016-12-10 20:47:34 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,8 +76,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    dog = Dog.new(name: row[1], breed: row[2])
-    dog.id = row[0]
+    dog = Dog.new(name: row[1], breed: row[2], id: row[0])
     dog
   end
 
